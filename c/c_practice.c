9cab8d46a7043f4f1b02b65a67ad18269736e007
Swansocl1 <120823142+Swansocl1@users.noreply.github.com>
2022-12-28 22:13:58 +0000

    adding switch function

--- c/c_practice.c
+++ c/c_practice.c
@@ -2,6 +2,20 @@
 
 int main() 
 {
-    printf("Hello World!\n");
+     printf("Hello World!\n");
     printf("I am learning C.\n");
+ 
+    int number = 2;
+    // evaluates each case then runs the appropriate one
+    switch (number) {
+        case 1:
+        printf("one\n");
+        break;
+        case 2:
+        printf("two\n");
+        break;
+        case 3:
+        printf("three\n");
+        break;
+}
 }
