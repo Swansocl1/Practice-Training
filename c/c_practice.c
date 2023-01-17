9160457461d6c9fc71999a352bd462c1fc4c0dd5
Swansocl1 <120823142+Swansocl1@users.noreply.github.com>
2023-01-17 01:45:52 +0000

    pointers

--- c/c_practice.c
+++ c/c_practice.c
@@ -64,4 +64,15 @@ printf("%d\n", x + y);
 int myAge = 43;
 printf("%p\n", &myAge);
 
+int* ptr = &myAge;  // A pointer variable, with the name ptr, that stores the address of myAge
+
+// Output the value of myAge (43)
+printf("%d\n", myAge);
+
+// Output the memory address of myAge (0x7ffe5367e044)
+printf("%p\n", &myAge);
+
+// Output the memory address of myAge with the pointer (0x7ffe5367e044)
+printf("%p\n", ptr);
+
 }
