31913560cd5d3d878c2a7d3a13b5bb2e24118670
Swansocl1 <120823142+Swansocl1@users.noreply.github.com>
2023-01-19 16:07:31 +0000

    taking an int as input without cs50 lib

--- c/c_practice.c
+++ c/c_practice.c
@@ -81,4 +81,16 @@ printf("%p\n", ptr);
 // Dereference
 printf("%d\n", *ptr);
 
+// Create an integer variable that will store the number we get from the user
+int myNum;
+
+// Ask the user to type a number
+printf("Type a number: \n");
+
+// Get and save the number the user types
+scanf("%d", &myNum);
+
+// Output the number the user typed
+printf("Your number is: %d\n", myNum);
+
 }
